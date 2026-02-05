83418ed9c8d4f2688130e1200d082a169c8cd2c4
rustbasic <127506429+rustbasic@users.noreply.github.com>
2026-02-05 16:05:46 +0900

    ezChat v0.1.2
    ezChat v0.1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// ezchat Version: v0.1.1
+// ezchat Version: v0.1.2
 // NOTE: Change the version number above whenever you update the app. 
 // This ensures the browser detects the change in sw.js and triggers a Service Worker update.
 
