988bb86436d1c993c96b84553e62ae285df5d909
rustbasic <127506429+rustbasic@users.noreply.github.com>
2026-02-08 21:56:04 +0900

    ezchat v0.1.9
    ezchat v0.1.9

---
+++ sw.js
@@ -0,0 +1,42 @@
+var cacheName = 'ezchat-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './ezchat.js',
+  './ezchat_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    }).then(() => {
+      return self.skipWaiting();
+    })
+  );
+});
+
+/* Clean up outdated caches and take control of all clients immediately */
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
