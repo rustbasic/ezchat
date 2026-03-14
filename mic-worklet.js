50307154935f9537fe84182485d7d5a70f46dd42
rustbasic <127506429+rustbasic@users.noreply.github.com>
2026-03-15 00:17:18 +0900

    ezChat v0.4.9
    ezChat v0.4.9

--- mic-worklet.js
+++ mic-worklet.js
@@ -1,32 +1,40 @@
 class MicProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
-    this.bufferSize = 8192;
-    this.accum = new Float32Array(this.bufferSize);
-    this.offset = 0;
+    this.bufferFrames = 8192;
+    this.channelCount = 1;
+    this.accum = new Float32Array(this.bufferFrames * this.channelCount);
+    this.offsetFrames = 0;
     this.stopped = false;
+    this.reported = false;
 
     this.port.onmessage = (event) => {
       const msg = event.data;
-      if (msg === 'flush') {
-        if (this.offset > 0) {
-          const tail = this.accum.subarray(0, this.offset);
-          const copy = new Float32Array(tail.length);
-          copy.set(tail);
-          this.port.postMessage(copy.buffer, [copy.buffer]);
-          this.offset = 0;
-        }
+
+      if (msg && msg.type === 'config') {
+        this.channelCount = Math.max(1, Math.min(2, msg.channelCount || 1));
+        this.accum = new Float32Array(this.bufferFrames * this.channelCount);
+        this.offsetFrames = 0;
+        this.reported = false;
+      } else if (msg === 'flush') {
+        this.flush();
       } else if (msg === 'stop') {
-        if (this.offset > 0) {
-          const tail = this.accum.subarray(0, this.offset);
-          const copy = new Float32Array(tail.length);
-          copy.set(tail);
-          this.port.postMessage(copy.buffer, [copy.buffer]);
-          this.offset = 0;
-        }
+        this.flush();
+        this.port.postMessage({ type: "stopped" });
         this.stopped = true;
       }
     };
+  }
+
+  flush() {
+    if (this.offsetFrames > 0) {
+      const used = this.offsetFrames * this.channelCount;
+      const tail = this.accum.subarray(0, used);
+      const copy = new Float32Array(tail.length);
+      copy.set(tail);
+      this.port.postMessage(copy.buffer, [copy.buffer]);
+      this.offsetFrames = 0;
+    }
   }
 
   process(inputs) {
@@ -35,24 +43,41 @@ class MicProcessor extends AudioWorkletProcessor {
     const input = inputs[0];
     if (!input || input.length === 0) return true;
 
-    const ch0 = input[0];
-    if (!ch0 || ch0.length === 0) return true;
+    const availableChannels = input.length;
+    const frames = input[0]?.length || 0;
+    if (frames === 0) return true;
 
-    let srcOffset = 0;
-    while (srcOffset < ch0.length) {
-      const writable = this.bufferSize - this.offset;
-      const readable = ch0.length - srcOffset;
-      const n = Math.min(writable, readable);
+    if (!this.reported) {
+      this.port.postMessage({
+        type: "channels",
+        inputChannelCount: availableChannels,
+        recordedChannelCount: this.channelCount
+      });
+      this.reported = true;
+    }
 
-      this.accum.set(ch0.subarray(srcOffset, srcOffset + n), this.offset);
-      this.offset += n;
-      srcOffset += n;
+    let srcFrame = 0;
+    while (srcFrame < frames) {
+      const writableFrames = this.bufferFrames - this.offsetFrames;
+      const readableFrames = frames - srcFrame;
+      const n = Math.min(writableFrames, readableFrames);
 
-      if (this.offset === this.bufferSize) {
+      for (let i = 0; i < n; i++) {
+        const dstBase = (this.offsetFrames + i) * this.channelCount;
+        for (let ch = 0; ch < this.channelCount; ch++) {
+          const src = ch < availableChannels ? input[ch] : input[0];
+          this.accum[dstBase + ch] = src[srcFrame + i];
+        }
+      }
+
+      this.offsetFrames += n;
+      srcFrame += n;
+
+      if (this.offsetFrames === this.bufferFrames) {
         const out = this.accum;
         this.port.postMessage(out.buffer, [out.buffer]);
-        this.accum = new Float32Array(this.bufferSize);
-        this.offset = 0;
+        this.accum = new Float32Array(this.bufferFrames * this.channelCount);
+        this.offsetFrames = 0;
       }
     }
 
